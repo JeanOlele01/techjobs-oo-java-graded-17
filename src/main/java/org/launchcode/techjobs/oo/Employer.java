1bb459818bf865ba7dca392644facd5015926db3
jeanOlele <84257608+jeanOlele@users.noreply.github.com>
2023-10-01 23:02:44 -0500

    completed DRYing the code

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class Employer extends JobField{
 
-//    private int ;
+//    private int id ;
 //    private static int nextId = 1;
 //    private String value;
 
